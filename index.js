4ac3aee17197b726701f9781351c2e56c3f31615
Ashfakul Islam <r4pido3@gmail.com>
2023-11-05 21:50:16 +0600

    food delete function added

--- index.js
+++ index.js
@@ -123,6 +123,14 @@ async function run() {
       }
     });
 
+    // delete food
+    app.delete('/api/myFood/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await foods.deleteOne(query);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
