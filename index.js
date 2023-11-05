47e4dbcdc7d00183c32c99dee26479404cabb780
Ashfakul Islam <r4pido3@gmail.com>
2023-11-05 11:45:43 +0600

    teams member data added to mongodb

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors())
+
+// MONGODB CONFIG
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.en41ppq.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+
+    //   await client.connect();
+        const database = client.db('PlatePals')
+        const teams = database.collection('teams')
+
+    // GET Team DATA
+    app.get('/api/teams', async (req, res) => {
+        try{
+            const cursor = teams.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        }
+        catch(err){
+            console.log(err);
+        }
+    })
+
+
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Welcome to PlatePals DATABASE')
